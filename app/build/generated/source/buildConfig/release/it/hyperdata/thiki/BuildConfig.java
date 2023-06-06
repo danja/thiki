5f96c47f6e80a7575f09a459c6f60244ce788abd
Danny Ayers <danny.ayers@gmail.com>
2023-06-06 02:13:42 +0200

    trying to get apk on phone

---
+++ app/build/generated/source/buildConfig/release/it/hyperdata/thiki/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package it.hyperdata.thiki;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "it.hyperdata.thiki";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "One Short Plank";
+}
